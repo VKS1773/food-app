4649bde51ac884b01199be109449c187ebc9f98f
Vikash Sharma <vs8824394430@gmail.com>
2024-07-12 00:59:03 +0530

    manage folder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#3333cc",
+        red: "#e60000",
+        primaryBG: "#f2f2f2",
+        secondary: "#555",
+      },
+    },
+  },
+  plugins: [],
+};
