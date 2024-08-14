e5601cafd5b440aebf8a0ba87beb1a8d57ef6009
Vikash Sharma <vs8824394430@gmail.com>
2024-08-14 17:15:30 +0530

    design categories

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { MdAddCall } from "react-icons/md";
+import React, { useContext, useEffect, useState } from "react";
+import logo from "../images/home/logo (2).png";
+// import { FaRegUser } from "react-icons/fa";
+// import Modal from "./Modal";
+// import { AuthContext } from "../contexts/AuthProvider";
+// import Profile from "./Profile";
+import { Link } from "react-router-dom";
+// import useCart from "../hooks/useCart";
+// import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
-  const navitem = (
+  const [isSticky, setSticky] = useState(false);
+  // const { user, loading } = useAuth();
+  // const [cart, refetch] = useCart();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const navItems = (
     <>
       <li>
         <a href="/">Home</a>
@@ -46,15 +73,19 @@ const Navbar = () => {
   );
   return (
     <>
-      <header className="mx-w-screen-2xl container mx-auto">
-        <div className="navbar bg-base-100">
+      <header
+        className={`max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out`}
+      >
+        <div
+          className={`navbar xl:px-24 ${
+            isSticky
+              ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out"
+              : ""
+          }`}
+        >
           <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
-              >
+            <div className="dropdown justify-between">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -69,40 +100,23 @@ const Navbar = () => {
                     d="M4 6h16M4 12h8m-8 6h16"
                   />
                 </svg>
-              </div>
+              </label>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-64 space-y-3"
               >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Parent</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Submenu 1</a>
-                    </li>
-                    <li>
-                      <a>Submenu 2</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
+                {navItems}
               </ul>
             </div>
-            <a to="/" className="btn btn-ghost text-xl">
-              LOGO
+            <a href="/">
+              <img src={logo} alt="" height="100" width="100" />
             </a>
           </div>
           <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">{navitem}</ul>
+            <ul className="menu menu-horizontal px-1">{navItems}</ul>
           </div>
-          <div className="navbar-end">
-            {/*search button */}
-            <button className="btn btn-ghost btn-circle mr-2 lg:flex hidden">
+          <div className="navbar-end ">
+            <button className="btn btn-ghost btn-circle hidden lg:flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -118,33 +132,52 @@ const Navbar = () => {
                 />
               </svg>
             </button>
-            {/*cart item */}
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle mr-3 lg:flex hidden flex items-center justify-center"
-            >
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-sm indicator-item">10</span>
-              </div>
-            </div>
-            <a className="btn bg-blue px-6  text-white rounded-full flex text-center gap-2">
-              <MdAddCall />
-            </a>
+
+            {/* shopping cart */}
+            <Link to="/cart-page">
+              <label
+                tabIndex={0}
+                className="btn btn-ghost btn-circle  lg:flex items-center justify-center mr-3"
+              >
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">
+                    {/* {cart.length || 0} */}
+                  </span>
+                </div>
+              </label>
+            </Link>
+
+            {/* login button */}
+
+            {/* {user ? (
+              <>
+                <Profile user={user} />
+              </>
+            ) : (
+              <button
+                onClick={() =>
+                  document.getElementById("my_modal_5").showModal()
+                }
+                className="btn flex items-center gap-2 rounded-full px-6 bg-green text-white"
+              >
+                <FaRegUser /> Login
+              </button>
+            )} */}
+            {/* <Modal /> */}
           </div>
         </div>
       </header>
